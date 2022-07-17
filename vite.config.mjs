3717376aa5cd9677a185324d00c6bf1ecc34b15d
Bjørn Madsen <bm@aeons.dk>
2022-07-17 15:28:59 +0200

    Update dependencies

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,9 +4,6 @@ import solidPlugin from "vite-plugin-solid";
 /** @type {import('vite').UserConfig} */
 export default defineConfig({
   plugins: [solidPlugin()],
-  server: {
-    port: 3000,
-  },
   build: {
     target: "esnext",
   },
